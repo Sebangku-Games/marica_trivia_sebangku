a843084cabd3ed23ba2e210a66d898c183311c63
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2023-08-21 09:50:11 +0700

    pertama

---
+++ Marica Quis Tes/Assets/Script/text_manager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class text_manager : MonoBehaviour
+{
+    public static text_manager instance; // Membuat instance yang dapat diakses dari semua scene
+    public string storedText; // Variabel untuk menyimpan teks
+                              
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Tetapkan objek ini ketika berpindah scene
+        }
+        else
+        {
+            Destroy(gameObject); // Hancurkan objek jika instance sudah ada
+        }
+    }
+
+    public void StoreText(string text)
+    {
+        storedText = text; // Simpan teks dari InputField
+    }
+
+    public string GetStoredText()
+    {
+        return storedText; // Ambil teks yang disimpan
+    }
+}
