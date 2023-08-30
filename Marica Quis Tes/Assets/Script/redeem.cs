a7326d1cdf70beeeb1fbe5e19403f500871fed20
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2023-08-30 10:16:26 +0700

    add redeem update

--- Marica Quis Tes/Assets/Script/redeem.cs
+++ Marica Quis Tes/Assets/Script/redeem.cs
@@ -8,6 +8,7 @@ public class redeem : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI buttonText;
     public GameObject prizePanel;
+    public GameObject notEnough;
 
 
     private void Start()
@@ -39,6 +40,27 @@ public class redeem : MonoBehaviour
             UpdateScoreDisplay(); // Update the displayed highscore
             prizePanel.SetActive(true);
             
+        }
+    }
+
+    public void tesRedeem()
+    {
+        if (PlayerPrefs.HasKey(GameUtility.SavePrefKey))
+        {
+            int highscore = PlayerPrefs.GetInt(GameUtility.SavePrefKey);
+
+            if (highscore >= 50)
+            {
+                highscore -= 50;
+                PlayerPrefs.SetInt(GameUtility.SavePrefKey, highscore);
+                PlayerPrefs.Save(); // Save the updated highscore
+                UpdateScoreDisplay(); // Update the displayed highscore
+            }
+            else
+            {
+                notEnough.SetActive(true);
+                Debug.Log("Not enough coins!"); // Print debug message if coins are not enough
+            }
         }
     }
 
